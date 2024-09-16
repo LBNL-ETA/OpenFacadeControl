b8ac20c2ca099cec0847669763a9cc9574433b79
StephenCzarnecki <sczarnecki@lbl.gov>
2024-09-16 16:27:57 -0400

    Added license and copyright.  Removed some unused files.

--- agents/ofc_web_agent/ofc_web_agent/ui/src/App.js
+++ agents/ofc_web_agent/ofc_web_agent/ui/src/App.js
@@ -1,3 +1,22 @@
+/*
+*** Copyright Notice ***
+
+OpenFacadeControl (OFC) Copyright (c) 2024, The Regents of the University
+of California, through Lawrence Berkeley National Laboratory (subject to receipt
+of any required approvals from the U.S. Dept. of Energy). All rights reserved.
+
+If you have questions about your rights to use or distribute this software,
+please contact Berkeley Lab's Intellectual Property Office at
+IPO@lbl.gov.
+
+NOTICE.  This Software was developed under funding from the U.S. Department
+of Energy and the U.S. Government consequently retains certain rights.  As
+such, the U.S. Government has been granted for itself and others acting on
+its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the
+Software to reproduce, distribute copies to the public, prepare derivative 
+works, and perform publicly and display publicly, and to permit others to do so.
+*/
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, provides basic styling
 import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
